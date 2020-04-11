cdc7145d819f18d1a95cc45f791990582b389c0f
Kopali <62541316+Kopali@users.noreply.github.com>
2020-04-12 00:00:37 +0800

    just d?

--- camera.js
+++ camera.js
@@ -34950,7 +34950,7 @@ parcelRequire = function(e, r, t, n) {
                             decodingMethod: "single-person"
                         });
                         d = d.concat(e), i = +y.singlePoseDetection.minPoseConfidence, c = +y.singlePoseDetection.minPartConfidence;
-                        document.getElementById('log').innerHTML=d.toString();
+                        document.getElementById('log').innerHTML=d;
                         
                         break;
                     case "multi-pose":
@@ -34962,7 +34962,7 @@ parcelRequire = function(e, r, t, n) {
                             nmsRadius: y.multiPoseDetection.nmsRadius
                         });
                         d = d.concat(n), i = +y.multiPoseDetection.minPoseConfidence, c = +y.multiPoseDetection.minPartConfidence;
-                        document.getElementById('log').innerHTML=d.toString();
+                        document.getElementById('log').innerHTML=d;
                 }
                 u.clearRect(0, 0, r, s), y.output.showVideo && (u.save(), u.scale(-1, 1), u.translate(-r, 0), u.drawImage(t, 0, 0, r, s), u.restore()), d.forEach(({
                     score: e,
