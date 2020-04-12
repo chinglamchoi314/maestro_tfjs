7a518ddc1a0dba0923fbb615155d57fc1ea0eb6c
Kopali <62541316+Kopali@users.noreply.github.com>
2020-04-12 23:50:03 +0800

    poseforccl=d

--- camera.js
+++ camera.js
@@ -34951,7 +34951,8 @@ parcelRequire = function(e, r, t, n) {
                         });
                         d = d.concat(e), i = +y.singlePoseDetection.minPoseConfidence, c = +y.singlePoseDetection.minPartConfidence;
                         document.getElementById('log').innerHTML=JSON.stringify(e[0]);
-                        
+                        poseforccl=d;
+
                         break;
                     case "multi-pose":
                         let n = await y.net.estimatePoses(t, {
@@ -34963,6 +34964,7 @@ parcelRequire = function(e, r, t, n) {
                         });
                         d = d.concat(n), i = +y.multiPoseDetection.minPoseConfidence, c = +y.multiPoseDetection.minPartConfidence;
                         document.getElementById('log').innerHTML=JSON.stringify(n[0]);
+                        poseforccl=d;
                 }
                 u.clearRect(0, 0, r, s), y.output.showVideo && (u.save(), u.scale(-1, 1), u.translate(-r, 0), u.drawImage(t, 0, 0, r, s), u.restore()), d.forEach(({
                     score: e,
