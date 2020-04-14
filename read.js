5f566d95dcc298de0f5f87fa9c932a994f34e146
Kopali <62541316+Kopali@users.noreply.github.com>
2020-04-14 22:32:17 +0800

    update

--- read.js
+++ read.js
@@ -1,5 +1,8 @@
 // i dont like javascript.
 // i dont know what i am doing.
 var trash;
-var x=document.getElementById('output').getImageData(0,0,trash.width,trash.height);
-html.getElementById('morelog')=x;
+var x
+function abc(){
+  x=document.getElementById('output').getImageData(0,0,trash.width,trash.height);
+  html.getElementById('morelog').innerHTML=x;
+}
