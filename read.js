fb3ec024f0e37fd9eede5f53345ea72c54468719
Kopali <62541316+Kopali@users.noreply.github.com>
2020-04-14 22:30:47 +0800

    get stuff from output

---
+++ read.js
@@ -0,0 +1,5 @@
+// i dont like javascript.
+// i dont know what i am doing.
+var trash;
+var x=document.getElementById('output').getImageData(0,0,trash.width,trash.height);
+html.getElementById('morelog')=x;
