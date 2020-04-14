bd44c931e1fc9d37c88ede151e6d2762ae2ef0eb
Kopali <62541316+Kopali@users.noreply.github.com>
2020-04-14 22:51:07 +0800

    Update read.js

--- read.js
+++ read.js
@@ -1,7 +1,7 @@
 // i dont like javascript.
 // i dont know what i am doing.
 var trash=getElementById('output');
-var x
+var x;
 function abc(){
   //x=document.getElementById('output').getImageData(0,0,trash.width,trash.height);
   document.getElementById('morelog').innerHTML=trash.width;
