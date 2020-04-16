98f4ecacd2a4a877ab76c17331435ad28358f6aa
Kopali <62541316+Kopali@users.noreply.github.com>
2020-04-16 21:35:45 +0800

    Update read.js

--- read.js
+++ read.js
@@ -1,8 +1,8 @@
 // i dont like javascript.
 // i dont know what i am doing.
-var trash=document.getElementById("output");
+var trash=document.getElementById("output").getContext("2d");
 var x;
 function abc(){
-  x=document.getElementById("output").getImageData(0,0,trash.width,trash.height);
-  document.getElementById("morelog").innerHTML=x;
+  //x=trash.getImageData(0,0,100,100);
+  document.getElementById("morelog").innerHTML=poseforccl;
 }
